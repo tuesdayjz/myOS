6388b648e543104683a742b7f962a6b70a7d94a0
tuesdayjz <moka010302@gmail.com>
2023-08-18 01:20:34 +0900

    add printf function

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ QEMU=qemu-system-riscv32
 # clang path
 CC=clang
 
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wexstra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
 # launch QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
